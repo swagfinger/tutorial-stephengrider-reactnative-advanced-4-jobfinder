391d35e04cd478c5cdeb562fc8d28e964fd558d6
Swag Finger <swagfinger@gmail.com>
2022-12-04 21:20:49 +0800

    push notifications - ExponentPushToken

--- App.js
+++ App.js
@@ -1,15 +1,49 @@
-import React from 'react';
-import { StyleSheet } from 'react-native';
+import React, { useState, useRef, useEffect } from 'react';
+import { Alert, StyleSheet } from 'react-native';
 import { Provider } from 'react-redux';
 
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 //import { PersistGate } from 'redux-persist/integration/react';
-
 // import { store, persistor } from './src/store';
+
+import * as Notifications from 'expo-notifications';
+import registerForPushNotificationsAsync from './src/services/push_notifications';
+
 import store from './src/store';
 import Navigation from './src/Navigation';
 
 export default function App() {
+  const [expoPushToken, setExpoPushToken] = useState('');
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then((token) => {
+      setExpoPushToken(token);
+    });
+
+    // This listener is fired whenever a notification is received while the app is foregrounded
+    notificationListener.current =
+      Notifications.addNotificationReceivedListener((notification) => {
+        setNotification(notification);
+        console.log('message: ', notification);
+      });
+
+    // This listener is fired whenever a user taps on or interacts with a notification (works when app is foregrounded, backgrounded, or killed)
+    responseListener.current =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log(response);
+      });
+
+    return () => {
+      Notifications.removeNotificationSubscription(
+        notificationListener.current
+      );
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
+
   return (
     <Provider store={store}>
       {/* <PersistGate loading={null} persistor={persistor}> */}
